405b86ec06158ac2079a47a2a09caaeaaf1f71cc
Joseph McCormick <esmevane@gmail.com>
2026-01-06 10:50:02 -0600

    Remove all code - clean break time.

--- crates/roguebench-protocol/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Network protocol and shared components for Roguebench.
-//!
-//! Contains components and messages that are replicated between
-//! client and server via Lightyear.
-
-pub mod prelude {
-    pub use roguebench_core::prelude::*;
-}
